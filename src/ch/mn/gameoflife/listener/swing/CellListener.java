50d90756b608e28ec533f1581109320c31348ecd
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-11-19 15:48:35 +0100

    made Serializable

--- src/ch/mn/gameoflife/listener/swing/CellListener.java
+++ src/ch/mn/gameoflife/listener/swing/CellListener.java
@@ -2,11 +2,13 @@ package ch.mn.gameoflife.listener.swing;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.Serializable;
 
 import ch.mn.gameoflife.model.Cell;
 import ch.mn.gameoflife.view.swing.SwingCell;
 
-public class CellListener extends MouseAdapter {
+public class CellListener extends MouseAdapter implements Serializable {
+	private static final long serialVersionUID = -6120715169654453325L;
 
 	@Override
 	public void mousePressed(MouseEvent me) {
