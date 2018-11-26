dc7c43e86284cf37ccc2254c953e40329eb7b307
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-11-26 08:30:57 +0100

    format

--- src/ch/mn/gameoflife/Main.java
+++ src/ch/mn/gameoflife/Main.java
@@ -4,14 +4,16 @@ import ch.mn.gameoflife.view.swing.SwingMainFrame;
 
 public class Main {
 
-	private static void initializeGame() {
-		SwingMainFrame mainFrame = new SwingMainFrame("Game of Life");
-		mainFrame.setVisible(true);
-		mainFrame.getGameThread().start();
-	}
-	
-	public static void main(String[] args) {
-		initializeGame();
-	}
+    private static void initializeGame() {
+
+        SwingMainFrame mainFrame = new SwingMainFrame("Game of Life");
+        mainFrame.setVisible(true);
+        mainFrame.getGameThread().start();
+    }
+
+    public static void main(String[] args) {
+
+        initializeGame();
+    }
 
 }
--- src/ch/mn/gameoflife/model/Cell.java
+++ src/ch/mn/gameoflife/model/Cell.java
@@ -3,25 +3,31 @@ package ch.mn.gameoflife.model;
 import java.io.Serializable;
 
 public class Cell implements Serializable {
-	private static final long serialVersionUID = 3880031941181526796L;
-	
-	boolean isAlive = false;
-	int aliveNeighbours;
-	
-	public boolean isAlive() {
-		return isAlive;
-	}
-	
-	public void setAlive(boolean isAlive) {
-		this.isAlive = isAlive;		
-	}
 
-	public int getAliveNeighbours() {
-		return aliveNeighbours;
-	}
+    private static final long serialVersionUID = 3880031941181526796L;
 
-	public void setAliveNeighbours(int aliveNeighbours) {
-		this.aliveNeighbours = aliveNeighbours;		
-	}
+    boolean isAlive = false;
+
+    int aliveNeighbours;
+
+    public boolean isAlive() {
+
+        return isAlive;
+    }
+
+    public void setAlive(boolean isAlive) {
+
+        this.isAlive = isAlive;
+    }
+
+    public int getAliveNeighbours() {
+
+        return aliveNeighbours;
+    }
+
+    public void setAliveNeighbours(int aliveNeighbours) {
+
+        this.aliveNeighbours = aliveNeighbours;
+    }
 
 }
