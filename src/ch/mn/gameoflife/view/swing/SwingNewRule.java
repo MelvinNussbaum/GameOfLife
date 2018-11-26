c59999ae4bd62a6f0c2047e8243bbee9e4a1c872
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-11-26 10:11:17 +0100

    implemented sendButton completly

--- src/ch/mn/gameoflife/view/swing/SwingNewRule.java
+++ src/ch/mn/gameoflife/view/swing/SwingNewRule.java
@@ -5,14 +5,18 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.text.ParseException;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.text.MaskFormatter;
+
+import ch.mn.gameoflife.listener.swing.RuleButtonListener;
 
 public class SwingNewRule extends JDialog {
 
@@ -29,6 +33,8 @@ public class SwingNewRule extends JDialog {
     private JPanel deadRuleGreaterPanel = new JPanel(new BorderLayout(10, 0));
 
     private JPanel deadRuleLessPanel = new JPanel(new BorderLayout(10, 0));
+
+    private JPanel buttonPanel = new JPanel(new BorderLayout(10, 0));
 
     private JLabel aliveRuleGreaterLabel = new JLabel(
         "<HTML><b>Lebende Zellen</b> bleiben am leben wenn Anzahl lebende Nachbarn <b>grösser als</b></HTML>: ");
@@ -50,10 +56,12 @@ public class SwingNewRule extends JDialog {
 
     private JFormattedTextField deadRuleLessField = new JFormattedTextField(maskFormatter);
 
+    private JButton sendRulesButton = new JButton("apply");
+
     public SwingNewRule(JFrame parent, boolean modal) throws ParseException {
         super(parent, modal);
         this.setTitle("Neue Regel");
-        this.setResizable(false);
+        this.setResizable(true);
         this.setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
 
         buildGUI();
@@ -63,20 +71,31 @@ public class SwingNewRule extends JDialog {
 
     private void buildGUI() {
 
+        sendRulesButton.setActionCommand("apply");
+        sendRulesButton.addActionListener(new RuleButtonListener(this));
+
         aliveRuleGreaterLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
         aliveRuleLessLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
         deadRuleGreaterLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
         deadRuleLessLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+        sendRulesButton.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 14));
 
         aliveRuleGreaterField.setPreferredSize(new Dimension(25, 25));
         aliveRuleLessField.setPreferredSize(new Dimension(25, 25));
         deadRuleGreaterField.setPreferredSize(new Dimension(25, 25));
         deadRuleLessField.setPreferredSize(new Dimension(25, 25));
 
+        aliveRuleGreaterPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
+        aliveRuleLessPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
+        deadRuleGreaterPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
+        deadRuleLessPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+
         aliveRuleGreaterPanel.add(aliveRuleGreaterLabel, BorderLayout.WEST);
         aliveRuleGreaterPanel.add(aliveRuleGreaterField, BorderLayout.EAST);
         aliveRuleLessPanel.add(aliveRuleLessLabel, BorderLayout.WEST);
         aliveRuleLessPanel.add(aliveRuleLessField, BorderLayout.EAST);
+        buttonPanel.add(sendRulesButton, BorderLayout.CENTER);
 
         deadRuleGreaterPanel.add(deadRuleGreaterLabel, BorderLayout.WEST);
         deadRuleGreaterPanel.add(deadRuleGreaterField, BorderLayout.EAST);
@@ -90,5 +109,48 @@ public class SwingNewRule extends JDialog {
         add(deadRuleGreaterPanel);
         add(Box.createRigidArea(new Dimension(0, 10)));
         add(deadRuleLessPanel);
+        add(Box.createRigidArea(new Dimension(0, 10)));
+        add(buttonPanel);
     }
+
+    public JFormattedTextField getAliveRuleGreaterField() {
+
+        return aliveRuleGreaterField;
+    }
+
+    public void setAliveRuleGreaterField(JFormattedTextField aliveRuleGreaterField) {
+
+        this.aliveRuleGreaterField = aliveRuleGreaterField;
+    }
+
+    public JFormattedTextField getAliveRuleLessField() {
+
+        return aliveRuleLessField;
+    }
+
+    public void setAliveRuleLessField(JFormattedTextField aliveRuleLessField) {
+
+        this.aliveRuleLessField = aliveRuleLessField;
+    }
+
+    public JFormattedTextField getDeadRuleGreaterField() {
+
+        return deadRuleGreaterField;
+    }
+
+    public void setDeadRuleGreaterField(JFormattedTextField deadRuleGreaterField) {
+
+        this.deadRuleGreaterField = deadRuleGreaterField;
+    }
+
+    public JFormattedTextField getDeadRuleLessField() {
+
+        return deadRuleLessField;
+    }
+
+    public void setDeadRuleLessField(JFormattedTextField deadRuleLessField) {
+
+        this.deadRuleLessField = deadRuleLessField;
+    }
+
 }
