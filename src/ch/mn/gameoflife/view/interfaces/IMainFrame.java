6d5a55799b4536836060399c172ad23422c2c4f8
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-11-27 10:51:38 +0100

    added a default method for internationalization

--- src/ch/mn/gameoflife/view/interfaces/IMainFrame.java
+++ src/ch/mn/gameoflife/view/interfaces/IMainFrame.java
@@ -1,10 +1,23 @@
 package ch.mn.gameoflife.view.interfaces;
 
-
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public interface IMainFrame {
-	
-	void buildGUI();
-	
-	void updateGUI();
+
+    void buildGUI();
+
+    void updateGUI();
+
+    default ResourceBundle internationalize() {
+
+        String language = Locale.getDefault().getLanguage();
+        String country = Locale.getDefault().getCountry();
+
+        Locale currentLocale = new Locale(language, country);
+
+        ResourceBundle rBundle = ResourceBundle.getBundle("MessagesBundle", currentLocale);
+
+        return rBundle;
+    }
 }
