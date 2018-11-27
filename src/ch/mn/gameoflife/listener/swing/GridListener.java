6c33736f420e180e372aee3b81257ad94028bd7c
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-11-27 16:24:43 +0100

    full implementation of the GridListener

--- src/ch/mn/gameoflife/listener/swing/GridListener.java
+++ src/ch/mn/gameoflife/listener/swing/GridListener.java
@@ -13,6 +13,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import ch.mn.gameoflife.model.Cell;
 import ch.mn.gameoflife.view.swing.SwingCell;
@@ -20,16 +21,32 @@ import ch.mn.gameoflife.view.swing.SwingCell;
 public class GridListener extends MouseAdapter {
 
     @Override
-    public void mousePressed(MouseEvent e) {
+    public void mousePressed(MouseEvent eP) {
+
+        this.execute(eP);
+        super.mousePressed(eP);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent eD) {
+
+        this.execute(eD);
+        super.mouseDragged(eD);
+    }
+
+    private void execute(MouseEvent e) {
 
         JPanel grid = (JPanel) e.getSource();
         JPanel swingCell = (JPanel) grid.getComponentAt(e.getPoint());
-        if (!(swingCell.getClass() == SwingCell.class)) {
+        if (swingCell == null || !(swingCell.getClass() == SwingCell.class)) {
             return;
         }
         Cell cell = ((SwingCell) swingCell).getCellModel();
-        cell.setAlive(!cell.isAlive());
-        super.mousePressed(e);
-    }
 
+        if (SwingUtilities.isLeftMouseButton(e)) {
+            cell.setAlive(true);
+        } else if (SwingUtilities.isRightMouseButton(e)) {
+            cell.setAlive(false);
+        }
+    }
 }
