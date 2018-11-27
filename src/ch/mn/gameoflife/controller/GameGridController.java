242fee85c6f210b54e905372169070247bc21d25
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-11-27 14:19:49 +0100

    format

--- src/ch/mn/gameoflife/controller/GameGridController.java
+++ src/ch/mn/gameoflife/controller/GameGridController.java
@@ -3,9 +3,11 @@ package ch.mn.gameoflife.controller;
 import java.io.Serializable;
 
 public class GameGridController implements Serializable {
-	private static final long serialVersionUID = -4225020371706191879L;
-	
-	public static final int GRIDCOLS = 100;
-	public static final int GRIDROWS = 100;
+
+    private static final long serialVersionUID = -4225020371706191879L;
+
+    public static final int GRIDCOLS = 100;
+
+    public static final int GRIDROWS = 100;
 
 }
