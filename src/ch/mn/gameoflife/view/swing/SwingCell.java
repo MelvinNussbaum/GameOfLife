f32b1487d0691a946ea34f346dd7ed975b04aa7c
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-11-28 17:00:42 +0100

    format

--- src/ch/mn/gameoflife/view/swing/SwingCell.java
+++ src/ch/mn/gameoflife/view/swing/SwingCell.java
@@ -8,25 +8,29 @@ import ch.mn.gameoflife.model.Cell;
 import ch.mn.gameoflife.view.interfaces.ICellView;
 
 public class SwingCell extends JPanel implements ICellView {
-	private static final long serialVersionUID = 1L;
-	
-	private Cell cellModel;
-	
-	public SwingCell(Cell cell) {
-		super();
-		this.cellModel = cell;
-	}
-	
-	@Override
-	public void repaintCell() {
-		setBackground(cellModel.isAlive() ? Color.WHITE : Color.BLACK);
-	}
 
-	public Cell getCellModel() {
-		return cellModel;
-	}
+    private static final long serialVersionUID = 1L;
 
-	public void setCellModel(Cell cellModel) {
-		this.cellModel = cellModel;
-	}
+    private Cell cellModel;
+
+    public SwingCell(Cell cell) {
+        super();
+        this.cellModel = cell;
+    }
+
+    @Override
+    public void repaintCell() {
+
+        setBackground(cellModel.isAlive() ? Color.WHITE : Color.BLACK);
+    }
+
+    public Cell getCellModel() {
+
+        return cellModel;
+    }
+
+    public void setCellModel(Cell cellModel) {
+
+        this.cellModel = cellModel;
+    }
 }
