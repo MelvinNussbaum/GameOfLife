eaf531179024fdb38eaf53b809e43ea8e8a70253
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-12-05 08:43:39 +0100

    removed language dependencies  from IMainFrame

--- src/ch/mn/gameoflife/view/interfaces/IMainFrame.java
+++ src/ch/mn/gameoflife/view/interfaces/IMainFrame.java
@@ -1,21 +1,8 @@
 package ch.mn.gameoflife.view.interfaces;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
 public interface IMainFrame {
 
     void buildGUI();
 
     void updateGUI();
-
-    default ResourceBundle switchLanguage(Locale currentLocale) {
-
-        ResourceBundle rBundle = ResourceBundle.getBundle("ch.mn.gameoflife.properties.MessagesBundle", currentLocale);
-        if (!(currentLocale.getLanguage().equals(rBundle.getLocale().getLanguage()))) {
-            currentLocale = new Locale("en");
-            rBundle = ResourceBundle.getBundle("MessagesBundle", currentLocale);
-        }
-        return rBundle;
-    }
 }
