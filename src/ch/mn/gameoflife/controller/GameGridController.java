eae08cfa04e34d035a40b43a5f23fa6330431ca6
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-12-05 08:52:40 +0100

    set to 10000 cells

--- src/ch/mn/gameoflife/controller/GameGridController.java
+++ src/ch/mn/gameoflife/controller/GameGridController.java
@@ -6,8 +6,8 @@ public class GameGridController implements Serializable {
 
     private static final long serialVersionUID = -4225020371706191879L;
 
-    public static final int GRIDCOLS = 150;
+    public static final int GRIDCOLS = 100;
 
-    public static final int GRIDROWS = 150;
+    public static final int GRIDROWS = 100;
 
 }
