230b862a49276ecd325e91c3fe1924e983e529c2
MelvinNussbaum <nussbaum.melvin@gmail.com>
2018-12-10 12:39:14 +0100

    added AbstractSafeManager.java for the Cell[][] cells variable

---
+++ src/ch/mn/gameoflife/persistance/AbstractSafeManager.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *
+ * [ AbstractSafeManager.java ]
+ *
+ * COPYRIGHT (c) 2002 - 2018 by Allianz-Suisse, Zürich, Switzerland.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Allianz-Suisse.
+ *
+ ******************************************************************************/
+package ch.mn.gameoflife.persistance;
+
+import ch.mn.gameoflife.model.Cell;
+
+public abstract class AbstractSafeManager implements ISafeManager {
+
+    protected Cell[][] cells;
+
+    public AbstractSafeManager() {
+
+    }
+
+    public Cell[][] getCells() {
+
+        return cells;
+    }
+
+    public void setCells(Cell[][] cells) {
+
+        this.cells = cells;
+    }
+
+}
