d03775857f7b856edbd5c2c5a8bd0d3048e5f577
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-01-28 15:21:19 +0100

    added javax.persistence annotations

--- src/ch/mn/gameoflife/model/Cell.java
+++ src/ch/mn/gameoflife/model/Cell.java
@@ -2,10 +2,20 @@ package ch.mn.gameoflife.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Cell implements Serializable {
 
     private static final long serialVersionUID = 3880031941181526796L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private boolean alive = false;
