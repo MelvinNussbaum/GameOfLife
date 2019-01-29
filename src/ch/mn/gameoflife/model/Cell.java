fe773ae8cecc89a68f2ec84b832472410a930670
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-01-29 09:41:47 +0100

    made attribute aliveNeighbours @Transient

--- src/ch/mn/gameoflife/model/Cell.java
+++ src/ch/mn/gameoflife/model/Cell.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table
@@ -20,6 +21,7 @@ public class Cell implements Serializable {
 
     private boolean alive = false;
 
+    @Transient
     private int aliveNeighbours;
 
     public Cell() {
