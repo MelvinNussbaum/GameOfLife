b299e010eb7f9a1cdf641793458833edc54ed8e9
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-01-30 11:07:28 +0100

    removed generated id

--- src/ch/mn/gameoflife/model/Cell.java
+++ src/ch/mn/gameoflife/model/Cell.java
@@ -3,8 +3,6 @@ package ch.mn.gameoflife.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -16,8 +14,7 @@ public class Cell implements Serializable {
     private static final long serialVersionUID = 3880031941181526796L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private long id;
 
     private boolean alive = false;
 
@@ -28,17 +25,17 @@ public class Cell implements Serializable {
         super();
     }
 
-    public Cell(int id) {
+    public Cell(long id) {
         super();
         this.id = id;
     }
 
-    public int getId() {
+    public long getId() {
 
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
 
         this.id = id;
     }
