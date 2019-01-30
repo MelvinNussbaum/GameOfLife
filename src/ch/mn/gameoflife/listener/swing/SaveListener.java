1522121ac001109b25a28d556fae70385f8f1f4a
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-01-30 11:11:57 +0100

    implemented SaveListener.java

--- src/ch/mn/gameoflife/listener/swing/SaveListener.java
+++ src/ch/mn/gameoflife/listener/swing/SaveListener.java
@@ -12,14 +12,24 @@ package ch.mn.gameoflife.listener.swing;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import ch.mn.gameoflife.model.Cell;
+import ch.mn.gameoflife.persistence.service.DatabaseManager;
+
 public class SaveListener implements ActionListener {
 
-    public SaveListener() {
+    private DatabaseManager dbManager;
 
+    private Cell[][] cells;
+
+    public SaveListener(Cell[][] cells) {
+        this.cells = cells;
+        this.dbManager = new DatabaseManager("Hibernate_JPA");
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
 
+        dbManager.createCellGrid();
+        dbManager.updateCells(this.cells);
     }
 }
