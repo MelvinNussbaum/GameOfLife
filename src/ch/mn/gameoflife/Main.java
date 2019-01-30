156c4b9686c94de5d96822ce180d9f8184d69c6a
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-01-30 11:10:57 +0100

    improved mvc

--- src/ch/mn/gameoflife/Main.java
+++ src/ch/mn/gameoflife/Main.java
@@ -1,5 +1,9 @@
 package ch.mn.gameoflife;
 
+import ch.mn.gameoflife.controller.CellController;
+import ch.mn.gameoflife.model.CellGrid;
+import ch.mn.gameoflife.model.GameModel;
+import ch.mn.gameoflife.thread.GameThread;
 import ch.mn.gameoflife.view.swing.SwingMainFrame;
 
 public class Main {
@@ -7,8 +11,24 @@ public class Main {
     private static void initializeGame() {
 
         SwingMainFrame mainFrame = new SwingMainFrame("Game of Life");
+        GameThread gameThread = new GameThread(mainFrame);
+        GameModel frameModel = new GameModel();
+        CellGrid cellGrid = new CellGrid();
+        CellController cellController = new CellController();
+
+        cellController.setCells(cellGrid.getCells());
+
+        gameThread.setModel(frameModel);
+        gameThread.setCellController(cellController);
+
+        mainFrame.setModel(frameModel);
+        mainFrame.setCellGrid(cellGrid);
+        mainFrame.setGameThread(gameThread);
+        mainFrame.buildGUI();
+
+        gameThread.start();
+
         mainFrame.setVisible(true);
-        mainFrame.getGameThread().start();
     }
 
     public static void main(String[] args) {
